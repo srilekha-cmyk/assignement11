1c6dce3cc97223301f8691f9ba3da6934d4ac581
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-27 18:41:30 +0530

    Create program26.py

---
+++ program26.py
@@ -0,0 +1,11 @@
+age=int(input("Enter age: "))
+if age<12:
+    print("Ticket Price = 100")
+elif age<=60:
+    print("Ticket Price = 200")
+else:
+    print("Ticket Price = 150")
+
+#Output
+#Enter age: 25
+#Ticket Price = 200
