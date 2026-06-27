4cfb71796be0db4dfdc29ebd0a712ed0be79007e
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-27 17:45:41 +0530

    Add basic calculator functionality in program12.py
    Implement basic calculator functionality with addition, subtraction, multiplication, and division.

---
+++ program12.py
@@ -0,0 +1,17 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+op=input("Enter operator: ")
+if op=="+":
+    print(a+b)
+elif op=="-":
+    print(a-b)
+elif op=="*":
+    print(a*b)
+elif op=="/":
+    print(a/b)
+
+#Output
+#Enter first number: 10
+#Enter second number: 5
+#Enter operator: +
+#15
