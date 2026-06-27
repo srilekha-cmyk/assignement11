0b05bd59805f00049021cbfb548b2478678972ce
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-27 18:23:47 +0530

    Create program16.py

---
+++ program16.py
@@ -0,0 +1,15 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+if a>b and a>c:
+    print(a)
+elif b>c:
+    print(b)
+else:
+    print(c)
+
+#Output
+#Enter first number: 10
+#Enter second number: 30
+#Enter third number: 20
+#30
