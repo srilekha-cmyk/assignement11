fd5f23fdf81733a1c6b9d7f3af8bea554f0f5303
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-27 18:26:02 +0530

    Create program17.py

---
+++ program17.py
@@ -0,0 +1,11 @@
+u=input("Enter username: ")
+p=input("Enter password: ")
+if u=="admin" and p=="1234":
+    print("Login Success")
+else:
+    print("Login Failed")
+
+#Output
+#Enter username: admin
+#Enter password: 1234
+#Login Success
