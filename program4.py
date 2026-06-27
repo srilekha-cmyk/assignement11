4bb6e99c4b6ceb06f449c8f6ce6823fb10f4e338
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-27 17:27:09 +0530

    Implement number comparison in program4.py
    Add a program to compare two numbers and print the larger one.

---
+++ program4.py
@@ -0,0 +1,11 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+if a>b:
+    print(a)
+else:
+    print(b)
+
+#Output
+#Enter first number: 10
+#Enter second number: 20
+#20
