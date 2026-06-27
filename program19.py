f2116ddad1ede73de4d0370cc92ca28f0188a503
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-27 18:28:42 +0530

    Create program19.py

---
+++ program19.py
@@ -0,0 +1,11 @@
+bal=int(input("Enter balance: "))
+amt=int(input("Enter amount: "))
+if amt<=bal:
+    print("Transaction Successful")
+else:
+    print("Insufficient Balance")
+
+#Output
+#Enter balance: 5000
+#Enter amount: 2000
+#Transaction Successful
