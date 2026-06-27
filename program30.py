5887f8df0636f091adcdf583afb60adfbb1293a9
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-27 18:48:57 +0530

    Create program30.py

---
+++ program30.py
@@ -0,0 +1,13 @@
+signal=input("Enter signal: ")
+if signal=="Red":
+    print("Stop")
+elif signal=="Yellow":
+    print("Wait")
+elif signal=="Green":
+    print("Go")
+else:
+    print("Invalid Signal")
+
+#Output
+#Enter signal: Green
+#Go
