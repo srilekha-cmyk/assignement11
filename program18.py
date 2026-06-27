00493db48ce64c21cba5574665b8c10f6a311b55
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-27 18:27:26 +0530

    Add program to check if a number is positive and even/odd

---
+++ program18.py
@@ -0,0 +1,12 @@
+n=int(input("Enter number: "))
+if n>0:
+    if n%2==0:
+        print("Positive Even")
+    else:
+        print("Positive Odd")
+else:
+    print("Not Positive")
+
+#Output
+#Enter number: 8
+#Positive Even
