acd7a6af2030c9cd7479a94e82094e89c7583ad9
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-27 17:48:40 +0530

    Add character type checker in program14.py

---
+++ program14.py
@@ -0,0 +1,13 @@
+ch=input("Enter character: ")
+if ch.isupper():
+    print("Uppercase")
+elif ch.islower():
+    print("Lowercase")
+elif ch.isdigit():
+    print("Digit")
+else:
+    print("Special Character")
+
+#Output
+#Enter character: A
+#Uppercase
